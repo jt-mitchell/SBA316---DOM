be03d5dbd83e112d37244a5145b5d5b4ea176634
Jeff Mitchell <jeffreytm@gmail.com>
2024-10-08 11:09:57 -0400

    main.js update

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.querySelector("h1").style.color"blue";
+//caches using getElementByID - creates message in HTML file.
+const top6mess = document.getElementById("top6");
+top6mess.textContent = "My Top Chessplayers:";
+
+document.querySelector("body").style.backgroundColor = "green";
